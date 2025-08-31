da48dc93a5c340692ad0a409d727a1c7d5423a39
Allenpt221 <Allenpt26@gmail.com>
2025-08-31 17:35:24 +0800

    portfolio-website

---
+++ portfolio-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+          <Route />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio-frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+function Header() {
+  return (
+    <div className="">Navbar</div>
+  )
+}
+
+export default Header
